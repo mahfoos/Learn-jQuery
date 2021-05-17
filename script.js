f03f384e69b9f73fcf1cf8c944067b47730d7d51
Mahfooz <umahfoosahamed@gmail.com>
2021-05-17 19:48:06 +0530

    updated

--- script.js
+++ script.js
@@ -29,5 +29,44 @@ $(document).ready(function(){
 
   $("#heading hed").append(tweet);
   $("#heading hed").prepend(tweet);
+
+
+  // Removing content 
+  //.empty() - empties the inner HTML of an element
+  //.remove() - removes the element completely
+
+  $(".button").empty();
+  $(".button").remove();
+
+
+  // Changeing atribute
+  //.removeAttr() - removes an attribute completely 
+  //.attr() - can read or set any attribute 
+
+  $("content img").removeAttr("alt"); // removing alt
+
+
+  // Css with jQuery
+  $("#social-nav").css("top", "-200px").css("left", "100px");
   
+  $("#social-nav").css({
+      "height" : "40",
+      "speed" : "100"
+  });
+
+  // Adding & Removing Classes
+  /*
+  removeClass() - remove a class from the matched elements
+  addClass() - adds a class to the matched elements
+  toggleClass - toggles the class on and off on the matched elements
+  */
+
+  $("header .wrapper").removeClass("wrapper");
+  $("header > div").removeClass("wrapper");
+
+  
+
+
+
+
 
